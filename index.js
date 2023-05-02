6235427a50e9192d99fa5cb7823bc58d8b84db9d
RohitMundhra123 <rohitm1924@gmail.com>
2023-05-02 08:13:31 +0530

    hi

--- index.js
+++ index.js
@@ -125,10 +125,21 @@ function isElementInViewport(element) {
   
 // ------------------------opening----------------------  
   
-window.addEventListener("load", function() {
-    document.querySelector(".loader").classList.add("loaded");
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
 });
-  
 
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el)=>observer.observe(el));
 
+const hiddenElements1 = document.querySelectorAll(".hidden1");
+hiddenElements1.forEach((el)=>observer.observe(el));
   
